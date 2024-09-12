3b5d421d785b5a23d4e16d30d8efa1d445fb6ade
Michael Lustig <lustig@knophy.com>
2024-09-12 19:58:53 -0400

    Initial Commit

---
+++ Demo Safari ExtensionTests/Demo_Safari_ExtensionTests.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_Safari_ExtensionTests.swift
+//  Demo Safari ExtensionTests
+//
+//  Created by laptop on 9/12/24.
+//
+
+import Testing
+@testable import Demo_Safari_Extension
+
+struct Demo_Safari_ExtensionTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Demo Safari ExtensionUITests/Demo_Safari_ExtensionUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Demo_Safari_ExtensionUITestsLaunchTests.swift
+//  Demo Safari ExtensionUITests
+//
+//  Created by laptop on 9/12/24.
+//
+
+import XCTest
+
+final class Demo_Safari_ExtensionUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
