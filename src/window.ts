402fe1f53a5ff51eff904a553d7ff184b82eb45a
Todd Littlejohn <toddlittlejohn@Mac.attlocal.net>
2024-09-26 15:26:18 -0400

    added storage

--- src/window.ts
+++ src/window.ts
@@ -69,6 +69,8 @@ export class RealGhost implements Ghost {
         // );
         console.log("Connecting... new");
 
+        window.localStorage.setItem("ghost-request", "connect")
+
         // First GET request to initiate connection
         // await fetch('https://b097-2600-1700-75c1-130-d860-67d6-ca2a-8ecf.ngrok-free.app/api/start')
         //     .then(response => response.json())
